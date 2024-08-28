4b388470f4f4aae27a5866c640b201ff986b50f7
ngan <vutran@sheridancollege.ca>
2024-08-28 11:04:17 -0400

    HTML is done for Post Add and Post List components. Working on Post class

---
+++ src/main/webapp/src/app/posts/post.ts
@@ -0,0 +1,8 @@
+export class Post {
+  id?: number;
+  creator?: string;
+  topic?: string;
+  content?: string;
+  dateCreated?: string;
+  timeCreated?: string;
+}
--- src/main/webapp/src/app/posts/posts.component.ts
+++ src/main/webapp/src/app/posts/posts.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { PostAddComponent } from './posts/post-add/post-add.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [PostAddComponent, PostListComponent],
   templateUrl: './posts.component.html',
-  styleUrl: './posts.component.css'
+  styleUrl: './posts.component.css',
 })
-export class PostsComponent {
-
-}
+export class PostsComponent {}
